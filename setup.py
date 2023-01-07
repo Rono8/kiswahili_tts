a93c2803e576834d0c9b37efa45dcd0b74bac241
Kiptoo Kelvin Rono <45574917+Rono8@users.noreply.github.com>
2023-01-07 18:00:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ if LooseVersion(pip.__version__) < LooseVersion("19"):
 # TODO(@dathudeptrai) update requirement if needed.
 requirements = {
     "install": [
-        "tensorflow-gpu==2.7.0",
-        "tensorflow-addons==0.16.1	",
+        "tensorflow-gpu==2.9.0",
+        "tensorflow-addons==0.19.0	",
         "setuptools>=38.5.1",
         "huggingface_hub==0.0.8",
         "librosa>=0.7.0",
